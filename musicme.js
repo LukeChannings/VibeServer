187626c83314abce10e4a2297492c174b7497aa8
Luke Channings <thefuzzball2005@gmail.com>
2011-12-22 23:36:07 +0000

    Initial commit.

---
+++ musicme.js
@@ -0,0 +1,126 @@
+// Dependencies.
+var sqlite = this.sqlite = require('sqlite3');
+var musicmetadata = require("musicmetadata");
+var fs = require("fs");
+var walk = require('walk');
+
+// Constructor.
+var MusicMe = function(callback){
+	
+	// Remember who you are.
+	var self = this;
+	
+	// Open a database.
+	var db = this.db = new sqlite.Database('musicme.db');
+	
+	// Turns the retrieved row into a member.
+	function assignMember(err,row){
+	
+		// If the row was successfully fetched:
+		if ( !err ) {
+		
+			// Make the setting a member.
+			self[row.id] = row.setting;
+		
+		}
+		
+		// If there was an error fetching the row, then throw it.
+		else throw err;
+		
+	}
+	
+	// Get settings, pass each row to assignMember and execute callback when finished.
+	db.each('SELECT * FROM settings',assignMember,function(){
+	
+		// Execute the callback, make it remember who it is!
+		callback.call(self);
+	
+	});
+
+}
+
+/**
+ * walkCollection
+ * @description Walks the collection_path and looks for audio files.
+ * @variable callback - function to be executed on completion, will be passed the array.
+ */
+MusicMe.prototype.walkCollection = function(callback){
+	
+	// Make sure there is a path.
+	if ( !this.collection_path ) throw 'There is no collection path. Fail.';
+	
+	// Remember who you are.
+	var self = this;
+	
+	// Make a walker.
+	var walker = walk.walk(this.collection_path);
+	
+	// Array for song paths.
+	var songs = [];
+	
+	// Check each file.
+	walker.on('file',function checkFile(path,stat,next){
+		
+		// Check for an audio file.
+		if ( /\.(mp(3|4)|aac|wav|flac|ogg)$/.test(stat.name) ){
+			
+			// Push the song path to the song array.
+			songs.push(path.replace(self.collection_path,'') + '/' + stat.name);
+		
+		}
+		
+		// Read next file.
+		next();
+		
+	});
+	
+	// When the walking has finished:
+	walker.on('end',function EOF(){
+		
+		// Execute the callback and pass it the songs array.
+		callback(songs);
+		
+	});
+	
+}
+
+/**
+ * getMetadata
+ * @description Gets the metadata for an audio file.
+ * @variable paths - array of paths to get the metadata for.
+ * @variable callback - function to execute when finished, passed an array of metadata objects.
+ */
+MusicMe.prototype.getMetadata = function(path,callback){
+	
+	var parser = new musicmetadata(fs.createReadStream(path));
+	
+	console.log(path);
+	
+	parser.on('done',function(metadata){
+		
+		callback(metadata);
+		
+	});
+}
+
+// MusicMe instance controller.
+new MusicMe(function controller(){
+	
+	// Can't have anyone telling us we're someone else.
+	var self = this;
+	
+	// Walk the collection and build a list of audio files.
+	self.walkCollection(function finishedWalking(paths){
+		
+		for ( var i = 0; i < 3; i++ )
+		{
+			// hand the array of audio files to getMetadata.
+			self.getMetadata((self.collection_path + paths[i]).replace(" ", "\ "),function(metadata){
+			
+				console.log(metadata);
+			
+			});
+		}
+	});
+
+});
