4b5fd19027c3a1bfc3c9938de0d7cb7ca139ecc1
Luke Channings <thefuzzball2005@gmail.com>
2011-12-27 02:21:14 +0000

    Added a setup script. (Only tested on OS X.)

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Edit this.
+MUSIC_COLLECTION='/Volumes/Media/Music'
+
+# Ignore all of this.
+
+rm musicme.db
+
+node app.js 2> /dev/null
+
+sqlite3 musicme.db "INSERT INTO settings (id,setting) VALUES('collection_path','$MUSIC_COLLECTION')"
+
+node app.js
